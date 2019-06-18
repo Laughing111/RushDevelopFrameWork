b2b8b0a03a2bb4ea22024fcd7f08bf935080c0f8
Laughing <244215913@qq.com>
2019-06-18 14:22:17 +0800

    fixed the cameraFunction

--- Assets/RushDevelopFW/Scripts/FunctionManager/CameraManager/CameraManager.cs
+++ Assets/RushDevelopFW/Scripts/FunctionManager/CameraManager/CameraManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour {
+public class CameraManager {
 
     private CameraManager() { }
     private WebCamDevice[] device;
